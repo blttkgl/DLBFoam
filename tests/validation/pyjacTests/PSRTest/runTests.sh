53e44df0e5b8b2944ec85606ddbf99e242434062
Morev Ilya <ilya.morev@aalto.fi>
2024-07-15 14:07:45 +0300

    Reduced ISAT code duplication and added "not implemented" error for reduction. Unfortunately, all the ISAT implementation is private in OpenFOAM, so ISAT class is copied almost completely still. Also, OpenFOAM's ISAT unfortunately offers only one constructor, initializing all the required data, do this data will be duplicated in the memory. A bit ugly, but in the lack of a proper interface I don't see other solution.

--- tests/validation/pyjacTests/PSRTest/runTests.sh
+++ tests/validation/pyjacTests/PSRTest/runTests.sh
@@ -48,6 +48,7 @@ fi
 cp testCase/constant/chemistryProperties testCase/constant/chemistryProperties.orig
 foamDictionary  testCase/constant/chemistryProperties -entry '#includeEtc' -add '"caseDicts/solvers/chemistry/TDAC/chemistryProperties.cfg"'
 foamDictionary  testCase/constant/chemistryProperties -entry tabulation -add '{}'
+foamDictionary  testCase/constant/chemistryProperties -expand -entry reduction -remove
 echo
 echo -e "${YELLOW}Test ISAT_pyJac compilation for DLBFoam-ISAT coupling:${DARKGRAY}"
 foamDictionary -entry tabulation/method -set ISAT_pyJac testCase/constant/chemistryProperties > /dev/null
